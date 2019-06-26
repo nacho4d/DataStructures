4a441d280e53309416557a6e296dc230667c1b22
Guillermo Ignacio Enriquez Gutierrez <ignacio@jp.ibm.com>
2019-06-26 17:42:41 +0900

    Add documentation for Quick sort

--- Sources/DataStructures/Sort.swift
+++ Sources/DataStructures/Sort.swift
@@ -76,9 +76,9 @@ extension Array {
 
     /// Insertion sort implementation. Insertion Sort is a simple sorting algorithm similar to when people manually sort cards in a bridge hand.
     ///
-    /// -  Note:Other algorithms generally run faster than insertion sort. It is much less efficient on large lists than more advanced algorithms such as quicksort, heapsort, or merge sort. However, insertion sort provides several advantages: Efficient for small data sets (much like other quadratic sorting algorithms), it can also be useful when input array is almost sorted, only few elements are misplaced in complete big array.Other advantage is it can sort a list as it receives it. It will not be efficient in the case of a reverse-ordered collection.
+    /// -  Note: Other algorithms generally run faster than insertion sort. It is much less efficient on large lists than more advanced algorithms such as quicksort, heapsort, or merge sort. However, insertion sort provides several advantages: Efficient for small data sets (much like other quadratic sorting algorithms), it can also be useful when input array is almost sorted, only few elements are misplaced in complete big array.Other advantage is it can sort a list as it receives it. It will not be efficient in the case of a reverse-ordered collection.
     ///
-    /// - Complexity:Worst-case and average time complexity is O(*n^2*)  where *n* is the length of the array. Best case time complexity is O(*n*) when array is already sorted. Space complexity is O(1).
+    /// - Complexity: Worst-case and average time complexity is O(*n^2*)  where *n* is the length of the array. Best case time complexity is O(*n*) when array is already sorted. Space complexity is O(1).
     ///
     /// **Example**
     ///
@@ -129,13 +129,47 @@ extension Array {
         sortInsertion(by: compare)
     }
 
+    // MARK: -
+
+    /// Quick sort implementation.  Quick Sort is a Divide and Conquer algorithm.
+    /// It picks an element as pivot and divides (partitions) the given array around the picked pivot. Then repeat the same for each partition until the entire array is sorted. In this implementation the middle element is chosen as the pivot. Hence it leads to
+    /// - Complexity: Worst-case and average time complexity is O(*n^2*)  where *n* is the length of the array. Best case time complexity is O(*n* log *n*) when array is already sorted. Space complexity is O(1).
+    /// -  Note: The key of Quick sort algorithm is pivot. Original Hoare's implementation chooses middle element which produces O(*n* log *n*) in best case. Lomuto's implementations chooses last element because is easier to implement and understand however it might lead to O(*n*^2) in best case. algorithms generally run faster than insertion sort. It is much less efficient on large lists than more advanced algorithms such as quicksort, heapsort, or merge sort. However, insertion sort provides several advantages: Efficient for small data sets (much like other quadratic sorting algorithms), it can also be useful when input array is almost sorted, only few elements are misplaced in complete big array.Other advantage is it can sort a list as it receives it. It will not be efficient in the case of a reverse-ordered collection.
+    ///
+    /// **Example**
+    ///
+    ///  Input is [ 5 1 4 2 8 ] then algorithm choses middle element (4) as pivot and partitions the array into two. Swap first left (search from left to right) item bigger than pivot with first element on the right smaller than pivot (search from right t,o left). Repeat until each partition cannot be partioned anymore (lenght is 1)
+    ///
+    ///  [ 5 1 **4** 2 8 ] → [ 2 1 4 ][ 5 8 ]
+    ///
+    ///  [ 2 **1** 4 ][ 5 8 ] → [ 1 2 ][ 4 ][ 5 8 ]
+    ///
+    ///  [ **1** 2 ][ 4 ][ 5 8 ]  → [ 1 ][ 2 ][ 4 ][ 5 8 ]
+    ///
+    ///  [ 1 ][ **2** ][ 4 ][ 5 8 ] → [ 1 ][ 2 ][ 4 ][ 5 8 ]
+    ///
+    ///  [ 1 ][ 2 ][ **4** ][ 5 8 ] → [ 1 ][ 2 ][ 4 ][ 5 8 ]
+    ///
+    ///  [ 1 ][ 2 ][ 4 ][ **5** 8 ] → [ 1 ][ 2 ][ 4 ][ 5 ][ 8 ]
+    ///
+    ///  [ 1 ][ 2 ][ 4 ][ **5** ][ 8 ] → [ 1 ][ 2 ][ 4 ][ 5 ][ 8 ]
+    ///
+    ///  [ 1 ][ 2 ][ 4 ][ 5 ][ **8** ] → [ 1 ][ 2 ][ 4 ][ 5 ][ 8 ], Finally Done!
+    ///
     public mutating func sortQuick(by compare: Comparator) {
         if count < 2 {
+            #if DEBUG
+            defer {
+                print(self)
+            }
+            #endif
             return
         }
         __sortQuick(low: 0, high: count - 1, by: compare)
     }
 
+    /// Recursive function, calls internally quick sort partition and divide the array into two groups.
+    /// Then call quick sort function again for each group.
     mutating func __sortQuick(low: Int, high: Int, by compare: Comparator) {
         if low < high {
             let p = __sortQuickPartition(low: low, high: high, by: compare)
@@ -144,7 +178,14 @@ extension Array {
         }
     }
 
+    /// Calculate partition at the middle of the given range.
+    /// Then compare each element in the range and divide it into 2 groups: smaller than pivot and bigger than pivot
     mutating func __sortQuickPartition(low: Int, high: Int, by compare: Comparator) -> Int {
+        #if DEBUG
+        defer {
+            print(self)
+        }
+        #endif
         let pivot = self[(low + high) / 2]
         var i = low - 1
         var j = high + 1
@@ -159,7 +200,6 @@ extension Array {
                 return j
             }
             swapAt(i, j)
-            //print(self)
         } while true
     }
 
